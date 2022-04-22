4263976b560c11d3ccbac1c75a7f28c90643a33b
Vit Onischenko <slonik0775@gmail.com>
2022-04-22 21:41:00 +0300

    mData transformation and random character component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,24 @@ _apiKey = 'apikey=12dc802311d5b78feee29b6e092de6dd';
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getRecource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getRecource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacters = (id) => {
-        return this.getRecource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+    getCharacter = async (id) => {
+      const res = await this.getRecource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+   return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description:char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this characret',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
